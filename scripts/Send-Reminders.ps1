8c449fe63a519001e94b1321a459f382ef7cf4c5
Philip Gai <philip-gai@github.com>
2022-03-28 11:56:32 -0500

    Update send_reminders script and workflow

---
+++ scripts/Send-Reminders.ps1
@@ -0,0 +1,37 @@
+$ErrorActionPreference = 'Stop'
+
+$repository = "philip-gai/philip-gai"
+
+Write-Host "Logging in..."
+gh auth login
+
+Write-Host "Getting issues..."
+$issues = $(gh issue list -R $repository --state open --json "number,assignees" -L 500) | ConvertFrom-Json
+Write-Host "Found $($issues.Count) issues"
+
+Write-Host "Filtering out issues that are not assigned..."
+$issues = $issues | Where-Object { $_.Assignees -ne $null -and $_.Assignees.Count -gt 0 }
+Write-Host "Found $($issues.Count) issues with assignees"
+
+foreach ($issue in $issues) {
+  $assigneeLogins = $issue.Assignees | Select-Object -ExpandProperty Login
+  $assigneesStr = $assigneeLogins -join ", "
+
+  $reminderMessage = ""
+            
+  $assigneeLogins | ForEach-Object {
+    $reminderMessage += "@$_ This is your friendly (automated) reminder!`n"
+  }
+
+  Write-Host "Sending reminder to $assigneesStr..."
+  gh issue comment $issue.number `
+    --body $reminderMessage `
+    -R $repository
+
+  if (!$?) {
+    Write-Warning "Failed to send reminder to $assignee"
+    continue
+  }
+}
+
+Write-Host "Done"
